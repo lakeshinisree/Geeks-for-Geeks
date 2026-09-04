e1d06b627d200f3b71f75486b3fb05b9b719d5d1
Kuntamukkala Lakeshini Sree <lakeshinisreekuntamukkala@gmail.com>
2026-09-04 18:55:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bird and Max Fruit Gathering/bird-and-max-fruit-gathering.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    def maxFruits(self, arr: list[int], m: int) -> int:
+        n = len(arr)
+        maxi = curr = sum(arr[:m])
+        for i in range(m, n):
+            curr += arr[i] - arr[i - m]
+            if curr > maxi:
+                maxi = curr
+        for i in range(m - 1):
+            curr += arr[i] - arr[-m + i]
+            if curr > maxi:
+                maxi = curr
+        return maxi
